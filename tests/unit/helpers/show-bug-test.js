d3985604f7626bedb81e763c237d1fcd4e5882aa
Lloyd Smith II <lloyd.s@socialcodeinc.com>
2016-08-13 15:32:26 -0400

    change tests to use more assertions: ok, notOk, equal

--- tests/unit/helpers/show-bug-test.js
+++ tests/unit/helpers/show-bug-test.js
@@ -6,9 +6,20 @@ test('it works with ember-qunit-nice-errors 0.1.0', function(assert) {
   let done = assert.async();
   assert.expect(10);
   setTimeout(() => {
-    for (let i = 0; i < 10; i++) {
-      assert.ok(true);
-    }
+  
+    assert.equal(true, true);
+    assert.notOk(false);
+    assert.ok(true);
+    
+    assert.equal(true, true);
+    assert.notOk(false);
+    assert.ok(true);
+    
+    assert.equal(true, true);
+    assert.notOk(false);
+    assert.ok(true);
+    
+    assert.equal('passes', 'passes');
     done();
   }, 0);
 });
